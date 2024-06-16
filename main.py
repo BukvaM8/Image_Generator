9ada5e4a01897290b9df69e2ceb9061655235db6
bukwawork <bukvawork@yandex.ru>
2024-06-16 14:49:33 +0300

    первый

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
 import streamlit as st
-
 import replicate
 
 st.title("# :rainbow[AI Image Generator]")
@@ -12,44 +10,39 @@ def configure_sidebar():
         with st.form("my_form"):
             width = st.number_input("Ширина картинки", min_value=256, max_value=2048, value=1024, step=16)
             height = st.number_input("Высота картинки", min_value=256, max_value=2048, value=1024, step=16)
-            promt = st.text_area("Введите промт...")
+            prompt = st.text_area("Введите промт...")
             submitted = st.form_submit_button("Отправить", type="primary")
             
             return {
                 "width": width, 
                 "height": height, 
-                "promt": promt, 
+                "prompt": prompt, 
                 "submitted": submitted,
-                }
-            
+            }
             
 def main_page(
     width: int,
     height: int, 
-    promt: str, 
+    prompt: str, 
     submitted: bool,
-    ):
+):
     if submitted:
         with st.spinner("Загрузка модели..."):
             result = replicate.run(
-                    "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
-                    input={
-                        "width": width,
-                        "height": height,
-                        "promt": promt,
-                    }
-
+                "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
+                input={
+                    "width": width,
+                    "height": height,
+                    "prompt": prompt,  # Исправление опечатки
+                }
             )
-            image = result[0]
+            image_url = result[0]  # Предполагается, что результат - это URL изображения
             with st.container(): 
-                st.image(image, caption="Ваше изображение")
+                st.image(image_url, caption="Ваше изображение")
                      
-            
 def main():
     data = configure_sidebar()
     main_page(**data)
     
-
-
 if __name__ == "__main__":
     main()
